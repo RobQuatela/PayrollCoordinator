ccf99073367b38e10169ae42b60779da0641d316
rquatela <rquatela@RQ-UBUNTU>
2017-04-04 17:02:31 -0400

    4/4/17

--- PayrollCoordinator/src/controller/MainController.java
+++ PayrollCoordinator/src/controller/MainController.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import com.opencsv.CSVReader;
 
@@ -12,7 +14,9 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -21,8 +25,10 @@ import javafx.stage.FileChooser;
 import model.EmployeeOriginal;
 import model.OriginPayData;
 
-public class MainController {
+public class MainController implements Initializable {
 
+    private ObservableList<EmployeeOriginal> originPayData = FXCollections.observableArrayList();
+	
     @FXML
     private Button btnImportOriginData;
     @FXML
@@ -37,8 +43,23 @@ public class MainController {
     private TableColumn<EmployeeOriginal, Double> tvOriginPayDataCOLregHours;
     @FXML
     private TableColumn<EmployeeOriginal, Double> tvOriginPayDataCOLotHours;
+    @FXML
+    private ComboBox<String> cbWeek;
     
-    private ObservableList<EmployeeOriginal> originPayData = FXCollections.observableArrayList();
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+    	cbWeek = new ComboBox<>();
+/*    	cbWeek.getItems().addAll(
+    			1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+    			13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+    			23, 24, 25, 26
+    			);*/
+    	cbWeek.getItems().addAll(
+    			"test",
+    			"test1"
+    			);
+	}
     
     
     public void btnImportOriginData_Clicked(ActionEvent event) {
@@ -74,4 +95,6 @@ public class MainController {
     	tvOriginPayDataCOLrate.setCellValueFactory(new PropertyValueFactory<EmployeeOriginal, Double>("originRate"));
     	tvOriginPayData.setItems(imports);
     }
+
+
 }
